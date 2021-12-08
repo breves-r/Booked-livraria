b141b008a78b100b7340a4e409722cef9989f292
Rafaela Breves <rafaela.curty13@gmail.com>
2021-12-07 21:24:24 -0300

    first commit

---
+++ applivraria/src/test/java/edu/infnet/applivraria/BookedApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.applivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
